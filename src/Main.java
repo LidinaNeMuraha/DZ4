a1164571ea71c90a9080dce84671ea8339f6872f
Александр <kmuken@bk.ru>
2024-02-26 21:52:53 +0500

    zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // zadanie 1
+
+        byte clientOS = 0;
+
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+
     }
 }
